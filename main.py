ace7e0f81082dd61d8274c440da73a4247b55c50
Julian-program <henryjulian.uce@gmail.com>
2024-12-13 21:56:05 -0500

    feat: :fire: Change a line

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola Mundo desde GitOps!"
+    return "Hola Mundo usando GitOps!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
